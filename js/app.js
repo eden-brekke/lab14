94dbf5680be07aab6ee8106ee2b5d0a59cfcd157
Tanesha Brester <taneshabrester@gmail.com>
2022-02-24 14:10:02 -0800

    troubleshooting

--- js/app.js
+++ js/app.js
@@ -8,10 +8,14 @@ const Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  let newCartItem = new CartItem (product, quantity);
+  this.items.push(newCartItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  let stringifyCart = JSON.stringify(cart);
+  localStorage.setItem('cart', stringifyCart);
 };
 
 Cart.prototype.removeItem = function(item) {
